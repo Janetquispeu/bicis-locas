760f3c86d48cd3e9728929ab550e95caa0909911
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-09-15 12:24:59 -0500

    corrigiendo apellido

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function validateForm(){
 		if(!valor.search(m)){
 	   		m=/[a-z]/g;
 	   		if(!valor.search(m)){
-	   			alert("La primera letra debe empezar con maýuscula")
+	   			return alert("La primera letra debe empezar con maýuscula")
 	   		}
 	   	}
 	  	else{
@@ -20,16 +20,24 @@ function validateForm(){
 	if( apellido == null || apellido.length == 0 || /^\s+$/.test(apellido) ) {
   	alert("Campo obligatorio Apellidos");
 	}
+	else{
+		var n = /^[a-zA-Z]*$/;
+		if(!apellido.search(n)){
+	   		n=/[a-z]/g;
+	   		if(!apellido.search(n)){
+	   			alert("La primera letra debe empezar con maýuscula")
+	   		}
+	   	}
+	  	else{
+  			alert ("Ingrese datos válidos (A-Z)");
+	  	}
+	}
 	var valor_1 = document.getElementById("input-email").value;
 	if( valor_1 == null || valor_1.length == 0 || !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valor_1)) {
        return alert("Ingresa un correo");
     }
 	var valor_2 = document.getElementById("input-password").value;
-	if( valor_2 == null || valor_2.length == 0 || /^\s+$/.test(valor_2) ) {
-		if(valor_2.length<6){
-  			alert("Ingrese al menos 6 caracteres");
-  		}
-  	
-	}
-    	
+	if( valor_2 == null || valor_2.length == 0 || valor_2.length <= 6 || valor_2 == "123456" || valor_2 == "98754" || valor_2 == "password") {
+        return alert("Contraseña no válida");
+    } 	
 }
